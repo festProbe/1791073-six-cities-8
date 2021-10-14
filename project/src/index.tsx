65fb8c9ca88859d1f31049a1d6ccd386c5902e7b
Евгений <festProbe@gmail.com>
2021-10-14 21:44:12 +1100

    Добавлены компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const AMSTERDAM_PLACES_COUNT = 5;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placesCount={AMSTERDAM_PLACES_COUNT} />
   </React.StrictMode>,
   document.getElementById('root'));
