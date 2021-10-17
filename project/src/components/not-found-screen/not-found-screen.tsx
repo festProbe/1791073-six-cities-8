5e9c081975ba5f1381b58fd68d7dae10ff87b236
Евгений <festProbe@gmail.com>
2021-10-17 12:18:31 +1100

    Настроил руты, добавил приватный рут, через лого можно вернуться на главную

--- project/src/components/not-found-screen/not-found-screen.tsx
+++ project/src/components/not-found-screen/not-found-screen.tsx
@@ -1,11 +1,8 @@
-import Card from '../card/card';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import Logo from '../logo/logo';
 
-type MainPageProps = {
-  placesCount: number;
-}
-
-function Main({ placesCount }: MainPageProps): JSX.Element {
+function NotFoundScreen(): JSX.Element {
   return (
     <>
       <div style={{ display: 'none' }}>
@@ -39,7 +36,7 @@ function Main({ placesCount }: MainPageProps): JSX.Element {
           </div>
         </header>
 
-        <main className="page__main page__main--index">
+        <main className="page__main page__main--index page__main--index-empty">
           <h1 className="visually-hidden">Cities</h1>
           <div className="tabs">
             <section className="locations container">
@@ -60,7 +57,7 @@ function Main({ placesCount }: MainPageProps): JSX.Element {
                   </a>
                 </li>
                 <li className="locations__item">
-                  <a className="locations__item-link tabs__item tabs__item--active" href="/">
+                  <a className="locations__item-link tabs__item" href="/">
                     <span>Amsterdam</span>
                   </a>
                 </li>
@@ -70,7 +67,7 @@ function Main({ placesCount }: MainPageProps): JSX.Element {
                   </a>
                 </li>
                 <li className="locations__item">
-                  <a className="locations__item-link tabs__item" href="/">
+                  <a className="locations__item-link tabs__item tabs__item--active" href="/">
                     <span>Dusseldorf</span>
                   </a>
                 </li>
@@ -78,36 +75,15 @@ function Main({ placesCount }: MainPageProps): JSX.Element {
             </section>
           </div>
           <div className="cities">
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{placesCount} places to stay in Amsterdam</b>
-                <form className="places__sorting" action="/" method="get">
-                  <span className="places__sorting-caption">Sort by</span>
-                  <span className="places__sorting-type" tabIndex={0}>
-                    Popular
-                    <svg className="places__sorting-arrow" width="7" height="4">
-                      <use xlinkHref="/icon-arrow-select"></use>
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                    <li className="places__option" tabIndex={0}>Price: low to high</li>
-                    <li className="places__option" tabIndex={0}>Price: high to low</li>
-                    <li className="places__option" tabIndex={0}>Top rated first</li>
-                  </ul>
-                </form>
-                <div className="cities__places-list places__list tabs__content">
-                  <Card></Card>
-                  <Card></Card>
-                  <Card></Card>
-                  <Card></Card>
-                  <Card></Card>
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">404 Page is not found</b>
+                  <p className="cities__status-description">We could not find page available at this address.</p>
+                  <Link className="locations__item-link" to={AppRoute.Main}>Back to Main Page</Link>
                 </div>
               </section>
-              <div className="cities__right-section">
-                <section className="cities__map map"></section>
-              </div>
+              <div className="cities__right-section"></div>
             </div>
           </div>
         </main>
@@ -116,4 +92,4 @@ function Main({ placesCount }: MainPageProps): JSX.Element {
   );
 }
 
-export default Main;
+export default NotFoundScreen;
---
+++ project/src/const.tsx
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Main = '/',
+  SignIn = '/sign-in',
+  Favorites = '/favorites',
+  Room = '/offer/:id'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
